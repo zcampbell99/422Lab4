ebd4a68e9d3cd4de2f92926b6837eacea3e00137
Zack Campbell <zack.c.campbell@utexas.edu>
2018-03-21 13:17:34 -0500

    Completed critter3 (Rabbit)

--- src/assignment4/Critter3.java
+++ src/assignment4/Critter3.java
@@ -7,10 +7,12 @@ public class Critter3 extends Critter {
 
     private static final int GENE_TOTAL = 24;
     private int dir;
+    private boolean didRep;
 
     public Critter3() {
         dir = Critter.getRandomInt(8);
         setEnergy(Params.start_energy);
+        didRep = false;
     }
 
     public boolean fight(String opponent) {
@@ -22,10 +24,21 @@ public class Critter3 extends Critter {
 
     @Override
     public void doTimeStep() {
-        if (Critter.getRandomInt(10) <= 3) {
+        if (Critter.getRandomInt(10) <= 3) {                    // More likely to run than walk
             updateLoc(dir, 1, ++numMoves);
         } else {
+            int temp = Params.run_energy_cost;                      // Running takes half as much energy
+            Params.run_energy_cost /= 2;
             run(dir);
+            Params.run_energy_cost = temp;
+        }
+        if (this.getEnergy() >= Params.min_reproduce_energy/2 && !didRep) {
+            didRep = true;
+            Critter3 child = new Critter3();
+            setEnergy(getEnergy()+(getEnergy()/4));
+            reproduce(child, Critter.getRandomInt(8));
+        } else {
+            didRep = false;
         }
     }
 }
